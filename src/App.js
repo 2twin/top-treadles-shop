b012c71f6ce8fc7c6eee013c2fe78962e49f2cc2
Илья Владимиров <flashwot@yandex.ru>
2022-01-31 19:03:32 +0300

    Разбивка CSS на модули

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import Card from "./components/Card";
+import Card from "./components/Card/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+const arr = [
+   { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.jpg' },
+   { title: 'Мужские Кроссовки Nike Air Max 270', price: 8499, imageUrl: '/img/sneakers/2.jpg'},
+   { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/3.jpg' },
+   { title: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: '/img/sneakers/4.jpg' }
+]
 
 function App() {
    return (
@@ -18,7 +24,15 @@ function App() {
             </div>
 
             <div className='d-flex'>
-               <Card /> 
+               {
+                  arr.map((obj) => (
+                     <Card 
+                        title={obj.title}
+                        price={obj.price}
+                        imageUrl={obj.imageUrl} 
+                        handleClick={() => alert(123)}/>
+                  ))
+               }
             </div>
          </div>
       </div>
